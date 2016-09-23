4d6c6a0f556e0b5a7d4cc6fd1e394ad297ce216c
ayuileng <631178386@qq.com>
2016-09-23 13:53:41 +0800

    finish TF

--- DataMining1/src/main/Main.java
+++ DataMining1/src/main/Main.java
@@ -6,16 +6,22 @@ import org.wltea.analyzer.core.Lexeme;
 
 import java.io.*;
 import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Atusleng_samA on 2016/9/20.
  */
 public class Main {
     private static Set<String> stopwords = new HashSet<>();
-     //每篇文章是一个String的list，每个子文件夹是list的list，于是整个语料库是list的list的list
-    //private static Map<List<List<String>>> articles = new ArrayList<>();
-    private static HashMap<Integer,HashMap<Integer,ArrayList<String>>> articles = new HashMap<>();
     private static List<File[]> dirs = new ArrayList<>();
+    private static Map<String,Integer> article = new TreeMap<>();//一篇文章
+    private static List<TreeMap<String,Integer>> articles = new ArrayList<>();//一个文件夹
+    private static Map<String,Integer> times = new HashMap<>();
+    private static TreeMap<String,HashMap<String,Integer>> TF = new TreeMap<>();
+    /**
+     * 读取stopwords到一个集合中
+     */
     public void readStopWords(){
         try {
             System.out.println("加载停止词...");
@@ -29,9 +35,11 @@ public class Main {
             System.out.println("停止词加载完毕,共有"+stopwords.size()+"个!");
         } catch (Exception e) {
             e.printStackTrace();
+            throw new RuntimeException("停止词加载失败");
         }
     }
-    public void readTxtWords(){
+
+    public void readAllFiles(){
         try {
             File filepath = new File("./files/");
             dirs = new ArrayList<>();
@@ -39,34 +47,45 @@ public class Main {
                 File[] files = filepath.listFiles()[i].listFiles();
                 dirs.add(files);
             }
-
-            for(int i=0;i<dirs.size();i++){
-                HashMap<Integer, ArrayList<String>> temp1=new HashMap<>();
-                for (int j=0;j<dirs.get(i).length;j++){
-                    ArrayList<String> temp = new ArrayList<>();
-                        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(dirs.get(i)[j]), "utf-8"));
-                        String line;
-                        while((line=reader.readLine())!=null){
-                            List<String> words = seprateWords(line);
-                            for (String word:
-                                 words  ) {
-                                if(word.length()>3&&!stopwords.contains(word)){
-                                    temp.add(word);
-
-                                    temp1.put(j,temp);
-                                    articles.put(i,temp1);
-                                }
-                            }
-                        }
-                }
-            }
-
-
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
+
+    public Map<String,Integer> readTFofOneArticle(File file){
+        Map<String,Integer> OneArticle = new HashMap<>();
+        String line;
+        try {
+            BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "utf-8"));
+            while ((line = reader.readLine()) != null) {
+                List<String> words = seprateWords(line);
+                for (String word : words) {
+                    if (word.length() > 3 && !stopwords.contains(word) && isWord(word)) {
+                        if(times.keySet().contains(word)){
+                            times.put(word,times.get(word)+1);
+                        }else{
+                            times.put(word,1);
+                        }
+                        if(OneArticle.keySet().contains(word)){
+                            OneArticle.put(word,OneArticle.get(word)+1);
+                        }else{
+                            OneArticle.put(word,1);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("读取一篇文章出错");
+        }
+        return OneArticle;
+    }
+    /**
+     * 将句子分词
+     * @param line
+     * @return
+     */
     public List<String> seprateWords(String line){
         List<String> words=new ArrayList<>();
         try {
@@ -80,26 +99,127 @@ public class Main {
         }
         return words;
     }
-    @Test
-    public void test(){
-        int sum=0;
-        readTxtWords();
-        for (Map.Entry e:
-             articles.entrySet()) {
-            for (Map.Entry e2:
-                    ((HashMap<Integer,ArrayList<String>>)e.getValue()).entrySet()) {
-                sum+=((ArrayList<String>)e2.getValue()).size();
+
+    /**
+     * 一共多少个文档：580
+     * @return
+     */
+    public int N() {
+        int sum = 0;
+        for (File[] fs :
+                dirs) {
+            sum+=fs.length;
+        }
+        return sum;
+    }
+
+    public int totalWords(Map<String,Integer> oneArticle){
+        int sum = 0;
+        for (Map.Entry<String,Integer> e:
+             oneArticle.entrySet()) {
+            sum+=e.getValue();
+        }
+        return sum;
+    }
+    public TreeMap<String,Double> getTF(Map<String,Integer> oneArticle){
+        int sum = totalWords(oneArticle);
+        TreeMap<String,Double> TF=new TreeMap<>();
+        for (Map.Entry<String,Integer> e :
+                oneArticle.entrySet()) {
+            TF.put(e.getKey(), 1+Math.log(e.getValue()));
+        }
+        return TF;
+    }
+
+    /**
+     * 判断是否是一个英文单词
+     * @param word
+     * @return
+     */
+    public boolean isWord(String word){
+        Pattern pattern = Pattern.compile("^[a-zA-Z]+$");
+        Matcher matcher = pattern.matcher(word);
+        return matcher.matches();
+    }
+    public int nTimes(List<TreeMap<String, Double>> TFs,String word){
+        int t=0;
+        for (TreeMap<String, Double> tf:
+             TFs) {
+            if(tf.keySet().contains(word)){
+                t++;
             }
         }
-        System.out.println(sum);
+        return t;
     }
+
     @Test
-    public void test2(){
-        List<String> strings = seprateWords("i was a boy");
-        for (int i = 0; i < strings.size(); i++) {
-            System.out.println(strings.get(i));
+    public void test(){
+        readStopWords();
+        readAllFiles();
+        List<TreeMap<String, Double>> TFs = new ArrayList<>();
+        for (File[] fs :
+                dirs) {
+            for (File file :
+                    fs) {
+                Map<String, Integer> article = readTFofOneArticle(file);
+                TreeMap<String, Double> tf = getTF(article);
+                TFs.add(tf);
+            }
         }
 
 
+//        for(String key:tf.keySet()){
+//            d+=tf.get(key);
+//            System.out.println("word:"+key+"的TF值是："+tf.get(key));
+//        }
+//        System.out.println(d);
+
+    }
+
+    @Test
+    public void test2(){
+        readAllFiles();
+        for (File[] fs :
+                dirs) {
+            for (File file :
+                    fs) {
+                readTFofOneArticle(file);
+            }
+        }
+//        for (Map.Entry e:
+//        times.entrySet()) {
+//            System.out.println(e.getKey()+" : "+e.getValue());
+//        }
+//        System.out.println(times.get("iloveyajima"));
+    }
+    @Test
+    public void test3(){
+        readStopWords();
+        readAllFiles();
+        Set<String> wordSet = new HashSet<>();
+        for (File[] fs :
+                dirs) {
+            for (File file :
+                    fs) {
+                try {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(file), "utf-8"));
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        List<String> words = seprateWords(line);
+                        for (String word :
+                                words) {
+                            if (word.length() > 3 && (!stopwords.contains(word)) && isWord(word)) {
+                                wordSet.add(word);
+                            }
+                        }
+                    }
+
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    throw new RuntimeException("读取一篇文章出错");
+                }
+            }
+        }
+        System.out.println(wordSet.size());
     }
 }
